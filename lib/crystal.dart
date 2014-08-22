9e612b25356694d2740342f89bcf6766c0db38c7
Alex Nichol <unixpickle@gmail.com>
2014-08-21 20:10:57 -0400

    abstract DpiMonitor interface

--- lib/crystal.dart
+++ lib/crystal.dart
@@ -4,4 +4,5 @@ import 'dart:async';
 import 'dart:html';
 
 part 'src/dpi_monitor.dart';
+part 'src/dpr_monitor.dart';
 part 'src/scalable_canvas.dart';
--- lib/src/dpi_monitor.dart
+++ lib/src/dpi_monitor.dart
@@ -1,49 +1,51 @@
 part of crystal;
 
-class DpiMonitor {
-  double _lastRatio;
-  StreamController _controller;
-  int _subCount = 0;
-  Timer _timer;
-  
-  Stream<double> get onChange => _controller.stream;
-  
-  bool get usesUpdateRequests => true;
-  
-  double get pixelRatio {
-    return _lastRatio;
+DpiMonitor _gMonitor = null;
+
+/**
+ * An interface for monitoring DPI changes.
+ */
+abstract class DpiMonitor {
+  /**
+   * Create or return an existing [DpiMonitor] subclass that is appropriate for
+   * the current browser.
+   */
+  factory DpiMonitor() {
+    if (_gMonitor == null) {
+      _gMonitor = new DprMonitor();
+    }
+    return _gMonitor;
   }
   
-  DpiMonitor() {
-    _lastRatio = window.devicePixelRatio;
-    _controller = new StreamController.broadcast(onListen: _onListen,
-        onCancel: _onCancel, sync: true);
-  }
+  /**
+   * A broadcast stream of DPI change events. Whenever the device's DPI changes,
+   * this stream will send the new [pixelRatio] over a stream.
+   */
+  Stream<double> get onChange;
   
+  /**
+   * Indicates if calling [requestUpdate] may serve to increase the speed of DPI
+   * change updates.
+   * 
+   * Some [DpiMonitor] subclasses work by periodically checking the DPI. If your
+   * application has a frequent timer or something of the sort, you may want to
+   * check for DPI updates on *that* timer.
+   */
+  bool get usesUpdateRequests => false;
+  
+  /**
+   * Returns the current device pixel ratio. This will only update after events
+   * are fired on [onChange], so this value may lag behind
+   * `window.devicePixelRatio` or some other value.
+   */
+  double get pixelRatio;
+  
+  /**
+   * If [usesUpdateRequests] is `true`, calling this will instruct the monitor
+   * to check if the DPI has changed. The [onChange] stream's listeners will be
+   * called before this method returns if the DPI has changed.
+   */
   void requestUpdate() {
-    _timerCb(null);
-  }
-  
-  void _onListen() {
-    ++_subCount;
-    if (_timer == null) {
-      _timer = new Timer.periodic(new Duration(milliseconds: 250), _timerCb);
-    }
-  }
-  
-  void _onCancel() {
-    if (--_subCount == 0) {
-      assert(_timer != null);
-      _timer.cancel();
-      _timer = null;
-    }
-  }
-  
-  void _timerCb(_) {
-    double ratio = window.devicePixelRatio;
-    if (ratio != _lastRatio) {
-      _lastRatio = ratio;
-      _controller.add(ratio);
-    }
+    
   }
 }
